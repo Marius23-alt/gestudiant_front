58dee387d365a6d4faddef7063892911f906d4f4
Alex-S06 <alexandre.soubils@toulouse.miage.fr>
2026-02-20 16:01:18 +0100

    Ajout des méthodes gestudiant -> gestudiant_front

--- front/src/main/java/fr/miage/toulouse/front/controller/DashboardController.java
+++ front/src/main/java/fr/miage/toulouse/front/controller/DashboardController.java
@@ -6,7 +6,11 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 
@@ -21,6 +25,46 @@ public class DashboardController {
     @FXML private TableColumn<Etudiant, String> colSemestre;
 
     private ObservableList<Etudiant> listeEtudiants = FXCollections.observableArrayList();
+
+    private MainController mainController;
+
+    /**
+     * Initialisation du mainControlleur pour pouvoir ensuite appeler ses méthodes (en particulier pour changement vue dashboard -> profilEtudiant
+     * @param mainController Un objet de type MainController
+     */
+    public void setMainController(MainController mainController){
+        this.mainController = mainController;
+    }
+
+    /**
+     * Configure la gestion des interactions souris sur le tableau des étudiants.
+     * Cette méthode définit une (RowFactory = comportement des lignes) personnalisée pour détecter les double-clics
+     * sur les lignes du tableau. Lorsqu'un étudiant est double-cliqué :
+     * - L'objet (Etudiant) correspondant à la ligne est récupéré
+     * - Une demande d'ouverture de la vue "Profil" est envoyée au contrôleur principal
+     * via (handleProfilEtudiant)
+     */
+    private void initialiserGestionDoubleClic() {
+        tableEtudiants.setRowFactory(tv -> {
+            TableRow<Etudiant> row = new TableRow<>();
+
+            row.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 2 && (!row.isEmpty())) {
+
+                    Etudiant etudiantSelectionne = row.getItem();
+
+                    if (mainController != null) {
+                        System.out.println("Ouverture du profil de : " + etudiantSelectionne.getNom());
+                        mainController.handleProfilEtudiant(etudiantSelectionne);
+                    } else {
+                        System.out.println("Erreur : MainController n'est pas lié !");
+                    }
+                }
+            });
+
+            return row;
+        });
+    }
 
     /**
      * Lie les colonnes du tableau aux attributs de la classe Etudiant puis appel chargerTableeau()
--- front/src/main/java/fr/miage/toulouse/front/controller/MainController.java
+++ front/src/main/java/fr/miage/toulouse/front/controller/MainController.java
@@ -4,6 +4,8 @@ package fr.miage.toulouse.front.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 
@@ -11,6 +13,8 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 
 public class MainController {
     private static final Logger LOGGER = Logger.getLogger(MainController.class.getName());
@@ -27,6 +31,23 @@ public class MainController {
      * @param fxmlFile
      * @param newTitle
      */
+
+    @FXML // Si souris passe sur bouton, changer la couleur du bouton
+    private void handleMouseIn(MouseEvent event) {
+        if (event.getSource() instanceof Button) {
+            Button btn = (Button) event.getSource();
+            btn.setStyle("-fx-background-color: #d30000; -fx-background-radius: 5; -fx-cursor: hand;");
+        }
+    }
+
+    @FXML // Si souris sort du bouton, remettre normal
+    private void handleMouseOut(MouseEvent event) {
+        if (event.getSource() instanceof Button) {
+            Button btn = (Button) event.getSource();
+            btn.setStyle("-fx-background-color: transparent;");
+        }
+    }
+
     private void loadView(String fxmlFile, String newTitle) {
         try {
             URL fxmlLocation = getClass().getResource("/fxml/" + fxmlFile);
@@ -49,10 +70,30 @@ public class MainController {
         }
 
     }
-
+    /**
+     * Charge et affiche la vue du tableau de bord (Dashboard).
+     * ELle permet de :
+     * - Charger le fichier FXML (dashboard.fxml)
+     * - Récupérer l'instance du DashboardController générée par le chargeur
+     * - Injecter la référence de ce contrôleur principal (this) dans le Dashboard via (setMainController)
+     * Cette liaison permet au Dashboard de demander l'ouverture d'autres vues (comme le profil étudiant) via le contrôleur principal.
+     */
     @FXML
     private void handleDashboard() {
-        loadView("dashboard.fxml", "Tableau de Bord");
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/dashboard.fxml"));
+            Parent view = loader.load();
+
+            DashboardController dashboardCtrl = loader.getController();
+            dashboardCtrl.setMainController(this);
+
+            contentArea.getChildren().setAll(view);
+            titleText.setText("Tableau de Bord");
+
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "Erreur chargement dashboard : {}", e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     @FXML
@@ -63,6 +104,37 @@ public class MainController {
     @FXML
     private void handleAdminSemestre() {
         loadView("adminSemestre.fxml", "Administration du Semestre");
+    }
+
+    /**
+     * Charge et affiche la vue détaillée du profil pour un étudiant donné.
+     * Elle permet de :
+     * - Charger le fichier FXML de la vue profil (profilEtudiant.fxml)
+     * - Récupérer le contrôleur associé et lui injecte l'objet étudiant (Tous les Controller sont créé automatiquement par JavaFX donc on a pas besoin de l'instancier, on récupère seulement l'instance)
+     * - Remplacer le contenu de la zone principale par cette nouvelle vue
+     * - Mettre à jour le titre de la fenêtre avec le nom et le prénom de l'étudiant
+     * En cas d'erreur lors du chargement du fichier FXML, une exception est loggée.
+     *
+     * @param etudiant L'objet sélectionné dont on souhaite visualiser les informations.
+     */
+    @FXML
+    public void handleProfilEtudiant(Etudiant etudiant) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/profilEtudiant.fxml"));
+            Parent view = loader.load();
+
+            ProfilEtudiantController profilCtrl = loader.getController();
+
+            profilCtrl.setEtudiant(etudiant);
+
+            contentArea.getChildren().setAll(view);
+            String titre = String.format("Profil de %s %s", etudiant.getNom(), etudiant.getPrenom());
+            titleText.setText(titre);
+
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "Erreur chargement profil : {}", e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     @FXML
